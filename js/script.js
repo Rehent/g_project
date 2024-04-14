c54011f9ffdad160fbbfb072af132883eed55a7e
Rehent <167021704+Rehent@users.noreply.github.com>
2024-04-14 23:08:16 +0300

    Homework 1 after lesson1

---
+++ js/script.js
@@ -0,0 +1,41 @@
+/* let neme = 'Иван';
+let city = 'Pavlograd';
+console.log(name);
+city = name;
+console.log(city); */
+
+/* let name = 'Olga';
+console.log(`привіт ${1}`);
+console.log(`привіт ${'name'}`);
+console.log(`привіт ${name}`); */
+
+/* let a = '5';
+let b = '13cvb';
+let c = '12.9sxdcfgv';
+console.log(typeof Number(a), typeof parseInt(b), typeof parseFloat(c)); */
+
+/* console.log((0.1 * 10 + 0.2 * 10) / 10); */
+
+//console.log(Math.max(20, 10, 50, 40));
+
+//console.log(Math.random() * (4 - 2) + 2);
+
+//console.log();
+/* const message = 'Welcome to Bahamas!';
+console.log(message.length);
+console.log(message.toUpperCase()); */
+
+/* let user = {};
+user.name = 'Ivan';
+user.age = 10;
+user.city = 'Kyiv';
+console.log(user);
+delete user.city;
+console.log(user);
+user['like flowers'] = true;
+console.log(user);
+
+for (key in user) {
+  console.log(key);
+  console.log(user[key]);
+} */
